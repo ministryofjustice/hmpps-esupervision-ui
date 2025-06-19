540aad2a1db3c7b62edb190f40e36ade46208bd7
jameskingtpx <james.king@tpximpact.com>
2025-06-19 13:47:12 +0100

    Image capture

--- server/app.ts
+++ server/app.ts
@@ -46,7 +46,7 @@ export default function createApp(services: Services): express.Application {
 
   app.use(routes(services))
   app.use('/submission', submissionRoutes())
-  app.use('/register', registerRoutes())
+  app.use('/register', registerRoutes(services))
 
   app.use((req, res, next) => next(createError(404, 'Not found')))
   app.use(errorHandler(process.env.NODE_ENV === 'production'))
--- server/routes/registerRoutes.ts
+++ server/routes/registerRoutes.ts
@@ -13,15 +13,21 @@ import {
   renderPhotoInform,
   renderPhotoReview,
 } from '../controllers/registerController'
+import { Services } from '../services'
 
 import { personalDetailsSchema, photoReviewSchema } from '../schemas/registerSchemas'
 
-export default function routes(): Router {
+export default function routes({ esupervisionService }: Services): Router {
   const router = Router({ mergeParams: true })
   const get = (routePath: string | string[], handler: RequestHandler) => router.get(routePath, asyncMiddleware(handler))
 
   get('/', renderIndex)
   router.post('/start', handleStart)
+
+  get('/test', async (req, res, next) => {
+    const currentDateTime = await esupervisionService.getCurrentTime()
+    return res.render('pages/register/index', { currentDateTime })
+  })
 
   get('/personal-details', renderPersonalDetails)
   router.post('/personal-details', validateFormData(personalDetailsSchema), handlePersonalDetails)

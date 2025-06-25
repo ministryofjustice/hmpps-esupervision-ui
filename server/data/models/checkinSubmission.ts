d3f118b05182cbb7c71d0479ffd976f5be23ad06
jameskingtpx <james.king@tpximpact.com>
2025-06-25 21:36:58 +0100

    Checkin API

--- server/data/models/checkinSubmission.ts
+++ server/data/models/checkinSubmission.ts
@@ -1,7 +1,5 @@
 export default class CheckinSubmission {
-  checkinUuid: string // TODO: require uuid
-
-  offenderUuid: string // TODO: require uuid
+  offender: string // TODO: require uuid
 
   answers: string // TODO: fix structure
 }

87f96d5ec8ba43531b304d4f291389199a175c34
jameskingtpx <james.king@tpximpact.com>
2025-07-02 14:37:47 +0100

    Consistent form values

--- server/utils/userFriendlyStrings.ts
+++ server/utils/userFriendlyStrings.ts
@@ -7,6 +7,17 @@ const definitions: Record<string, string> = {
   WEEKLY: 'Every week',
   TWO_WEEKS: 'Every 2 weeks',
   FOUR_WEEKS: 'Every 4 weeks',
+  VERY_WELL: 'Very well',
+  WELL: 'Well',
+  NOT_GREAT: 'Not great',
+  STRUGGLING: 'Struggling',
+  MENTAL_HEALTH: 'Mental health',
+  ALCOHOL: 'Alcohol',
+  DRUGS: 'Drugs',
+  MONEY: 'Money',
+  SUPPORT_SYSTEM: 'Support system',
+  OTHER: 'Other',
+  NO_HELP: 'No, I do not need help',
 }
 
 export default function getUserFriendlyString(key: string): string {
